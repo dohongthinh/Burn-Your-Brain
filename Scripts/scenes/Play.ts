92d801c83af21f0c6f89c1b1ecaae6aeae6888f7
Yi Lin Zhao <zyilin1998@outlook.com>
2020-03-29 17:42:50 -0400

    Swap Professor asset

--- Scripts/scenes/Play.ts
+++ Scripts/scenes/Play.ts
@@ -32,10 +32,10 @@ module scenes
             this.test2 = new createjs.Bitmap("./Assets/Images/openNotebook.png");
             this.testObject = new objects.classroomItem("./Assets/Images/openNotebook.png", 470, 240, true, this.test, this.test2);
             this.player1 = new objects.Character("./Assets/Images/Char1/tile000.png", 50, 240, true);
-            this.player2 = new objects.Character("./Assets/Images/Char2/tile000.png",100,150,true);
+            this.player2 = new objects.Character("./Assets/Images/Pro000.png",100,150,true);
             this.score = new objects.Label("Score:","20px", "Arial", "#000000", 15,30,false);
             this.dog1 = new objects.Dog("./Assets/Images/Dog-L.png",200,40,true);
-            this.table1 = new objects.classroomObstacle("./Assets/Images/Amiya1.png",300,200,true);
+            this.table1 = new objects.classroomObstacle("./Assets/Images/Small_square_table.png",300,200,true);
             //start timer
             this.timer = new objects.timer(21); //time in seconds
             this.timerLabel = new objects.Label("Time left: ", "20px", "Arial", "#000000", 15 , 10, false);
