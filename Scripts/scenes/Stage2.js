84f1583928a32b08f30b04165e651ddda45df4a8
dohongthinh <dohongthinhdht1999@gmail.com>
2020-03-29 20:34:59 -0400

    Increased the time

--- Scripts/scenes/Stage2.js
+++ Scripts/scenes/Stage2.js
@@ -40,7 +40,7 @@ var scenes;
             this.table5 = new objects.classroomObstacle("./Assets/Images/Small_square_table.png", 500, 280, true);
             this.table6 = new objects.classroomObstacle("./Assets/Images/Small_square_table.png", 500, 410, true);
             //start timer
-            this.timer = new objects.timer(21); //time in seconds
+            this.timer = new objects.timer(46); //time in seconds
             this.timerLabel = new objects.Label("Time left: ", "20px", "Arial", "#000000", 15, 10, false);
             config.Game.PLAYER = this.player1;
             this.Main();
